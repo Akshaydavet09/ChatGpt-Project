6717f615e33156544d84f17682fec7a4e738a3c1
Akshaydavet09 <akshay.bca2326@bpibs.in>
2026-06-13 23:00:05 +0530

    done

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,7 +1,21 @@
 import "./ChatWindow.css"
 function ChatWindow(){
     return <div className="chat-window">
-        <span className="chat-window-heading">Akshay</span>
+        <div className="navbar">
+        <span className="chat-window-heading">A-GPT<i class="fa-solid fa-angle-down"></i></span>
+        <div className="profile"><i class="fa-solid fa-user"></i>
+        </div>
+        </div>
+         <div className="chat-input">
+            <div className="input">
+                <input type="text" placeholder="Ask Anything"/>
+                <div className="send-btn">
+                <i class="fa-solid fa-paper-plane"></i>
+                </div>
+
+            </div>
+            <p>A-GPT can make mistakes. Check important info.</p>
+        </div>
 
     </div>
 }
